0e4758be6e1027be620e3617367ec502cf47a67d
Francis Setash <francis@francisseash.com>
2017-11-06 20:34:26 -0500

    Contiued work towards making this run as a Windows Service

--- route53ddns.go
+++ route53ddns.go
@@ -22,6 +22,9 @@ type Config struct {
 	SecretKey string
 }
 
+//Configuration - global configuration
+var Configuration Config
+
 // Service Stuff
 var logger service.Logger
 
@@ -35,47 +38,15 @@ func (p *program) Start(s service.Service) error {
 	return nil
 }
 func (p *program) run() {
-	configureMode := flag.Bool("configure", false, "Use this flag to configure AWS credentials")
-	flag.Parse()
-	//Parse config file to Config struct
-	//conf.toml should live in the same directory as the compiled exe
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-	exePath := filepath.Dir(ex)
-
-	c, err := ioutil.ReadFile(exePath + "/conf.toml")
-	if err != nil {
-		fmt.Println("Error Reading Config")
-		os.Exit(1)
-	}
-	var conf Config
-	if _, err := toml.Decode(string(c), &conf); err != nil {
-		fmt.Print("Cannot decode conf.toml")
-		os.Exit(1)
-	}
-
-	// Decodes config file access key and sets temp environment variable for S3 actions
-	accessKey, _ := base64.StdEncoding.DecodeString(conf.AccessKey)
-	secretKey, _ := base64.StdEncoding.DecodeString(conf.SecretKey)
-	os.Setenv("AWS_ACCESS_KEY_ID", string(accessKey))
-	os.Setenv("AWS_SECRET_ACCESS_KEY", string(secretKey))
-
-	if *configureMode {
-		fmt.Println("Configure Mode")
-		configure(conf)
-		os.Exit(0)
-	}
 
 	currentIP, err := checkIP()
-	createRecord(conf, currentIP)
+	createRecord(Configuration, currentIP)
 	var ip string
 
 	for {
 		ip, err = checkIP()
 		if currentIP != ip {
-			createRecord(conf, ip)
+			createRecord(Configuration, ip)
 			currentIP = ip
 			fmt.Println("Updated IP")
 
@@ -100,14 +71,50 @@ func (p *program) Stop(s service.Service) error {
 
 func main() {
 
+	configureMode := flag.Bool("configure", false, "Use this flag to configure AWS credentials")
+	installMode := flag.Bool("install", false, "Use this flag to install the Windows Service")
+	uninstallMode := flag.Bool("uninstall", false, "Use this flag to uninstall the Windows Service")
+	flag.Parse()
+	//Parse config file to Config struct
+	//conf.toml should live in the same directory as the compiled exe
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exePath := filepath.Dir(ex)
+
+	c, err := ioutil.ReadFile(exePath + "/conf.toml")
+	if err != nil {
+		fmt.Println("Error Reading Config")
+		os.Exit(1)
+	}
+	//var conf Config
+	if _, err := toml.Decode(string(c), &Configuration); err != nil {
+		fmt.Print("Cannot decode conf.toml")
+		os.Exit(1)
+	}
+
+	// Decodes config file access key and sets temp environment variable for S3 actions
+	accessKey, _ := base64.StdEncoding.DecodeString(Configuration.AccessKey)
+	secretKey, _ := base64.StdEncoding.DecodeString(Configuration.SecretKey)
+	os.Setenv("AWS_ACCESS_KEY_ID", string(accessKey))
+	os.Setenv("AWS_SECRET_ACCESS_KEY", string(secretKey))
+
+	if *configureMode {
+		fmt.Println("Configure Mode")
+		configure(Configuration)
+		os.Exit(0)
+	}
+
 	svcConfig := &service.Config{
-		Name:        "Route53DDNS",
-		DisplayName: "Route53DDNS",
+		Name:        "R53DDNSSRV",
+		DisplayName: "Route53DDNS Service",
 		Description: "Service for the Purpose of Performing DDNS to Route53",
 	}
 
 	prg := &program{}
 	s, err := service.New(prg, svcConfig)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -115,6 +122,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if *installMode {
+		fmt.Println("Install Mode")
+		s.Install()
+		os.Exit(0)
+	}
+	if *uninstallMode {
+		fmt.Println("Uninstall Mode")
+		s.Stop()
+		time.Sleep(5 * time.Second)
+		s.Uninstall()
+		logger.Info("uninstalled")
+		os.Exit(0)
+	}
+
+	//s.Install() to install
+	//s.Uninstall() to uninstall
 	err = s.Run()
 	if err != nil {
 		logger.Error(err)
